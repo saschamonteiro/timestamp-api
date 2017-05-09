83314c34a5bc91b977fc45defa449bf2b9fb1a62
Sascha Monteiro <sascha.monteiro@gmail.com>
2017-05-09 06:12:29 +0000

    1st attempt

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express')
+var moment = require('moment')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:dateobj', function (req, res) {
+  console.log(req.params.dateobj)
+  var m = moment(req.params.dateobj)
+   res.setHeader('Content-Type', 'application/json');
+   var y = parseInt(req.params.dateobj, 10);
+   if(!isNaN(y)){
+       m = moment.unix(y)
+       res.send(JSON.stringify({ unix: m.format('X') , natural: m.format('MMMM DD, YYYY')}))
+   } else if(m.isValid()){
+    res.send(JSON.stringify({ unix: m.format('X') , natural: m.format('MMMM DD, YYYY')}))
+  }else{
+    res.send(JSON.stringify({ unix: null , natural: null}))
+  }
 })
 
 app.listen(8080, function () {
